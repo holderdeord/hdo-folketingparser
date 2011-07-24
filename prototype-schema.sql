5a67f45d2b5b8eed57ebaecadfe81e282b33a6a8
Petter Reinholdtsen <pere@hungry.com>
2011-07-24 18:03:15 +0200

    Add tables for office holders.

--- prototype-schema.sql
+++ prototype-schema.sql
@@ -11,6 +11,44 @@ CREATE TABLE person_ref (
   ref text
 );
 
+CREATE TABLE office_entered_reason_type (
+  reason text
+);
+INSERT INTO office_entered_reason_type VALUES ('unknown');
+INSERT INTO office_entered_reason_type VALUES ('general_election');
+INSERT INTO office_entered_reason_type VALUES ('by_election');
+INSERT INTO office_entered_reason_type VALUES ('changed_party');
+INSERT INTO office_entered_reason_type VALUES ('reinstated');
+
+CREATE TABLE office_left_reason_type (
+  reason text
+);
+INSERT INTO office_left_reason_type VALUES ('unknown');
+INSERT INTO office_left_reason_type VALUES ('still_in_office');
+INSERT INTO office_left_reason_type VALUES ('general_election');
+INSERT INTO office_left_reason_type VALUES ('general_election_standing');
+INSERT INTO office_left_reason_type VALUES ('general_election_not_standing');
+INSERT INTO office_left_reason_type VALUES ('changed_party');
+INSERT INTO office_left_reason_type VALUES ('died');
+INSERT INTO office_left_reason_type VALUES ('declared_void');
+INSERT INTO office_left_reason_type VALUES ('resigned');
+INSERT INTO office_left_reason_type VALUES ('disqualified');
+INSERT INTO office_left_reason_type VALUES ('became_peer');
+
+CREATE TABLE office_holder (
+  person_id int NOT NULL REFERENCES person(id),
+  party varchar(100),
+  office_name varchar(100) NOT NULL,
+  constituency varchar(100),
+
+  entered_house date NOT NULL DEFAULT '1000-01-01',
+  left_house date NOT NULL DEFAULT '9999-12-31',
+  entered_reason NOT NULL REFERENCES office_entered_reason_type(reason)
+                 DEFAULT 'unknown',
+  left_reason NOT NULL REFERENCES office_left_reason_type(reason)
+                 DEFAULT 'unknown'
+);
+
 CREATE TABLE division (
   id int PRIMARY KEY NOT NULL,
   description text,
